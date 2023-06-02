840cb90e735dd3b671289ae44afbc7a1b92baa35
MikelMC1 <128159120+MikelMC1@users.noreply.github.com>
2023-06-02 09:57:29 +0200

    Add files via upload
    source code

---
+++ main.py
@@ -0,0 +1,72 @@
+
+
+import sys
+from functools import partial
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+ERROR_MSG = "ERROR"
+WINDOW_SIZE = 290
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40
+
+
+class PyCalcWindow(QMainWindow):
+
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyCalc")
+        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        self.buttonMap = {}
+        buttonsLayout = QGridLayout()
+        keyBoard = [
+            ["7", "8", "9", "/", "C", "√"],
+            ["4", "5", "6", "*", "(", "log"],
+            ["1", "2", "3", "-", ")", "ln"],
+            ["0", "00", ".", "+", "=", "sin"],
+        ]
+
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+                self.buttonMap[key] = QPushButton(key)
+                self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttonsLayout.addWidget(self.buttonMap[key], row, col)
+
+        self.generalLayout.addLayout(buttonsLayout)
+
+
+def main():
+
+    pycalcApp = QApplication([])
+    pycalcWindow = PyCalcWindow()
+    pycalcWindow.show()
+
+
+
+if __name__ == "__main__":
+    main()
